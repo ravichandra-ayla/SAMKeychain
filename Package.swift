51e609d36d89cc9cf9bd7f9d7d85f7208ae73550
Ravichandra KR <ravichandra.kr@aylanetworks.com>
2025-04-07 14:46:12 +0530

    [New] Added Package to support SPM

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.5
+//
+//  Package.swift
+
+import PackageDescription
+
+let package = Package(name: "SAMKeychain",
+                      platforms: [.macOS(.v10_10),
+                                  .iOS(.v9),
+                                  .tvOS(.v9),
+                                  .watchOS(.v2)],
+                      products: [.library(name: "SAMKeychain",
+                                          targets: ["SAMKeychain"])],
+                      targets: [.target(name: "SAMKeychain",
+                                        path: "Sources",
+                                        publicHeadersPath: "")])
